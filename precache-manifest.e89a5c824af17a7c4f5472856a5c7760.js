1bb1f91b6ac71219c94f0223ff2aa16b1e9bf213
Luicery <56919859+Luicery@users.noreply.github.com>
2020-02-27 12:52:19 +1100

    Updates

---
+++ precache-manifest.e89a5c824af17a7c4f5472856a5c7760.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8daffcf70914536ba154ee2d068095ba",
+    "url": "/Final-Project-Ecommerce-React/index.html"
+  },
+  {
+    "revision": "748420016073f3962baa",
+    "url": "/Final-Project-Ecommerce-React/static/css/main.596bc991.chunk.css"
+  },
+  {
+    "revision": "bcce9b9d78854acb757c",
+    "url": "/Final-Project-Ecommerce-React/static/js/2.5e98477f.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Final-Project-Ecommerce-React/static/js/2.5e98477f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "748420016073f3962baa",
+    "url": "/Final-Project-Ecommerce-React/static/js/main.76490c78.chunk.js"
+  },
+  {
+    "revision": "6983d1a10e7346557bd5",
+    "url": "/Final-Project-Ecommerce-React/static/js/runtime-main.f2406f41.js"
+  },
+  {
+    "revision": "1d6032b690beba94c4698ff6feb15bf2",
+    "url": "/Final-Project-Ecommerce-React/static/media/cart.1d6032b6.png"
+  },
+  {
+    "revision": "5eff281369c8a6cb205ecb0ac724ead6",
+    "url": "/Final-Project-Ecommerce-React/static/media/reviewStar.5eff2813.png"
+  },
+  {
+    "revision": "a22110a36a204df6e9a0c40e7cabd8c8",
+    "url": "/Final-Project-Ecommerce-React/static/media/reviewStarEmpty.a22110a3.png"
+  },
+  {
+    "revision": "073863582a85941a9ff2533676907528",
+    "url": "/Final-Project-Ecommerce-React/static/media/wishlist.07386358.png"
+  }
+]);
